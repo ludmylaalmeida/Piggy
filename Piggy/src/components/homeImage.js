ca43f1af79e4b7c0319d43861f4634c62c304b4a
Victoria Mom <victoria.j.mom@gmail.com>
2021-03-19 23:04:46 -0400

    Home image has been added

--- Piggy/src/components/homeImage.js
+++ Piggy/src/components/homeImage.js
@@ -1,12 +1,12 @@
 import React from "react"
 import HomePageImage from "../assets/images/home_page_illustration.svg"
+import piggy from './Piggy.png'
 
 export default function homeImage() {
   return (
     <div>
       <p>
-        What do I like to do? Lots of course but definitely enjoy building
-        websites.
+      <img src={piggy} width="800vw" height="550vh"/>
       </p>
     </div>
   )
