bdfde3f2b62e1aad482ab3bac19817bfd0864e99
gui-student <r4kgry0w6lg54ap@gmail.com>
2021-04-12 19:44:27 -0400

    Create home dashboard create post

--- src/components/dashboardCreatePost.js
+++ src/components/dashboardCreatePost.js
@@ -1,8 +1,30 @@
 import React from "react"
-import InputBase from "@material-ui/core/InputBase"
-import { Box, makeStyles, Grid, Typography, fade } from "@material-ui/core"
+import {
+  Box,
+  makeStyles,
+  Grid,
+  Typography,
+  fade,
+  Paper,
+  Modal,
+} from "@material-ui/core"
 import PostMedia from "../assets/images/create-post.svg"
 import UserIcon from "../assets/images/user-icon.svg"
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10
+}
+
+function getModalStyle() {
+  const top = 50 + rand()
+  const left = 50 + rand()
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  }
+}
 
 const useStyles = makeStyles(theme => ({
   dashboardGridWrap: {
@@ -33,38 +55,58 @@ const useStyles = makeStyles(theme => ({
       marginLeft: theme.spacing(3),
       width: "auto",
     },
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
+    color: "#8E8E93",
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
   },
+
   postMediaWrap: {
     marginTop: theme.spacing(1),
     marginLeft: theme.spacing(3),
     width: 280,
   },
   userIconWrap: {
+    marginLeft: theme.spacing(2),
+  },
+  paperWrap: {
     marginLeft: theme.spacing(3),
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(80),
+      height: theme.spacing(10),
+    },
   },
 }))
 
 export default function DashboardCreatePost() {
   const classes = useStyles()
+  const [modalStyle] = React.useState(getModalStyle)
+  const [open, setOpen] = React.useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const body = (
+    <div style={modalStyle} className={classes.search}>
+      <h2 id="simple-modal-title">Create Post</h2>
+      <p id="simple-modal-description">What's on your mind?</p>
+    </div>
+  )
+
   return (
     <div className={classes.dashboardGridWrap}>
       <Box pt={1} display="flex">
         <Grid container spacing={2}>
           <Grid item xs={2}>
-            <Box flexGrow={1}>Left Sidebar Here</Box>
+            <Box flexGrow={1}></Box>
           </Grid>
           <Grid item xs={7}>
             <div className={classes.middleWrap}>
@@ -77,44 +119,51 @@ export default function DashboardCreatePost() {
                 >
                   You have two new messages!
                 </Typography>
-                <Box pt={1} display="flex">
-                  <Grid container spacing={1}>
-                    <Grid item xs={1}>
-                      <Box flexGrow={1}>
-                        <img
-                          src={UserIcon}
-                          className={classes.userIconWrap}
-                          alt="User Icon"
-                        />
-                      </Box>
-                    </Grid>
-                    <Grid item xs={11}>
-                      <Box flexGrow={1}>
-                        <div className={classes.search}>
-                          <InputBase
-                            placeholder="Start a post"
-                            classes={{
-                              root: classes.inputRoot,
-                              input: classes.inputInput,
-                            }}
-                            inputProps={{ "aria-label": "search" }}
+                <Paper className={classes.paperWrap} elevation={0}>
+                  <Box pt={1} display="flex">
+                    <Grid container spacing={1}>
+                      <Grid item xs={1}>
+                        <Box flexGrow={1}>
+                          <img
+                            src={UserIcon}
+                            className={classes.userIconWrap}
+                            alt="User Icon"
                           />
-                        </div>
-                        <img
-                          src={PostMedia}
-                          className={classes.postMediaWrap}
-                          alt="photo, video, document"
-                        />
-                      </Box>
+                        </Box>
+                      </Grid>
+                      <Grid item xs={11}>
+                        <Box flexGrow={1}>
+                          <div
+                            className={classes.search}
+                            type="button"
+                            onClick={handleOpen}
+                          >
+                            Start a post
+                            <Modal
+                              open={open}
+                              onClose={handleClose}
+                              aria-labelledby="simple-modal-title"
+                              aria-describedby="simple-modal-description"
+                            >
+                              {body}
+                            </Modal>
+                          </div>
+                          <img
+                            src={PostMedia}
+                            className={classes.postMediaWrap}
+                            alt="photo, video, document"
+                          />
+                        </Box>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </Box>
+                  </Box>
+                </Paper>
               </Box>
             </div>
           </Grid>
           <Grid item xs={3}>
             <div className={classes.rightWrap}>
-              <Box flexGrow={1}>Right Sidebar Here</Box>
+              <Box flexGrow={1}></Box>
             </div>
           </Grid>
         </Grid>
